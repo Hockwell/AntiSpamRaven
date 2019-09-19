c1424580d91e2fc9f3a7fd31e29853eedf08730f
Vladimir Volnukhin <volnukhin.vladimir.gen@ya.ru>
2019-09-19 22:12:01 +0300

    [v.0.0.0] ReCreate project

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('lalka')
+import pandas as pd
+import numpy as np
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
+from sklearn.metrics import f1_score, accuracy_score
+
+vis_data = pd.read_csv(r"C:\Users\volnu\OneDrive\Data\Edu\ML\2 Регрессии\train.csv", encoding = 'ISO-8859-1', low_memory = False)
+data = vis_data[['fine_amount', 'state_fee', 'late_fee', 'discount_amount', 'balance_due', 'compliance']].dropna()
